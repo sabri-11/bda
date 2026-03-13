84bcc9cb1cbe8881805aabc2053b8bbfcbd9e041
Sabri <sabrirebaine@gmail.com>
2026-03-13 16:17:24 +0100

    fin de l'exercice 2

--- tp1/Requêtes sur la base.sql
+++ tp1/Requêtes sur la base.sql
@@ -74,3 +74,83 @@ FROM student, takes, course
 WHERE student.ID = takes.ID
 AND takes.course_id = course.course_id
 AND course.dept_name = 'Comp. Sci.'
+
+-- 14
+SELECT DISTINCT student.name
+FROM student, takes, teaches, teacher
+WHERE student.ID = takes.ID
+AND takes.course_id = teaches.course_id
+AND takes.sec_id = teaches.sec_id
+AND takes.semester = teaches.semester
+AND takes.year = teaches.year
+AND teaches.ID = teacher.ID
+AND teacher.name = 'Einstein';
+
+-- 15
+SELECT teaches.course_id, teacher.name
+FROM teaches, teacher
+WHERE teaches.ID = teacher.ID;
+
+-- 16
+SELECT course_id, sec_id, COUNT(ID)
+FROM takes
+WHERE semester = 'Spring'
+AND year = 2010
+GROUP BY course_id, sec_id;
+
+-- 17
+SELECT dept_name, MAX(salary)
+FROM teacher
+GROUP BY dept_name;
+
+-- 18
+SELECT course_id, sec_id, semester, year, COUNT(ID)
+FROM takes
+GROUP BY course_id, sec_id, semester, year;
+
+-- 19
+SELECT building, COUNT(*)
+FROM section
+WHERE (semester = 'Fall' AND year = 2009)
+OR (semester = 'Spring' AND year = 2010)
+GROUP BY building;
+
+-- 20
+SELECT course.dept_name, COUNT(*)
+FROM course, section, department
+WHERE course.course_id = section.course_id
+AND course.dept_name = department.dept_name
+AND section.building = department.building
+GROUP BY course.dept_name;
+
+-- 21
+SELECT DISTINCT course.title, teacher.name
+FROM course, section, teaches, teacher
+WHERE course.course_id = section.course_id
+AND section.course_id = teaches.course_id
+AND section.sec_id = teaches.sec_id
+AND section.semester = teaches.semester
+AND section.year = teaches.year
+AND teaches.ID = teacher.ID;
+
+-- 22
+SELECT semester, COUNT(*)
+FROM section
+WHERE semester IN ('Summer', 'Fall', 'Spring')
+GROUP BY semester;
+
+-- 23
+SELECT student.name, SUM(course.credits)
+FROM student, takes, course
+WHERE student.ID = takes.ID
+AND takes.course_id = course.course_id
+AND course.dept_name != student.dept_name
+GROUP BY student.name;
+
+-- 24
+SELECT department.dept_name, SUM(course.credits)
+FROM course, section, department
+WHERE course.course_id = section.course_id
+AND course.dept_name = department.dept_name
+AND section.building = department.building
+GROUP BY department.dept_name;
